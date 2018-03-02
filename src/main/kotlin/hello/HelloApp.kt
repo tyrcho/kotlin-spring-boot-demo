87f3f7f3249dfa92a1c3b226b4b93d781ba007cc
Michel Daviot <michel@daviot.info>
2018-03-02 12:45:56 +0100

    :bug: Kotlin app need to be non final (open).

--- src/main/kotlin/hello/HelloApp.kt
+++ src/main/kotlin/hello/HelloApp.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class HelloApp
+open class HelloApp
 
 fun main(args: Array<String>) {
     SpringApplication.run(HelloApp::class.java, *args)
